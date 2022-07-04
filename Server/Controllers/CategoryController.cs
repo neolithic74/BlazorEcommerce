ace1de45c9aa7b2983dfac8def1a33f451b4277b
Marco Brancaleoni <marco.brancaleoni@intercomp.it>
2022-07-04 22:11:46 +0200

    Modify Category

--- Server/Controllers/CategoryController.cs
+++ Server/Controllers/CategoryController.cs
@@ -16,7 +16,7 @@ namespace BlazorEcommerce.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<ServiceResponse<CategoryController>>> GetCategories()
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategories()
         {
             var result = await categoryService.GetCategories();
             return Ok(result);
